94f0a75516df569b48ae6974b5ad3d9029e99a08
anthow <anthonyenglebert@gmail.com>
2022-09-15 19:43:37 +0200

    /

--- src/templates/classes.js
+++ src/templates/classes.js
@@ -14,6 +14,9 @@ const CLassePage  = ({ data })  => (
 
   <section>
     <h1 className="text-3xl font-black text-red-600">{data.datoCmsClasse.nomDeLaClasse}</h1>
+    <h2 className="text-2xl font-black text-yellow-600"> Background</h2>
+    <div className="w-1/2 mb-10" dangerouslySetInnerHTML={{ __html: data.datoCmsClasse.background }} />
+
     <h2 className="text-2xl font-black text-yellow-600"> capacité spéciale</h2>
     <h3 className="text-xl font-black text-white my-5"> {data.datoCmsClasse.nomDeLaCapacitSpCiale}</h3>
 
@@ -31,6 +34,7 @@ query classsePageQuery($url: String){
     nomDeLaClasse
     nomDeLaCapacitSpCiale
     dTailCapacitSpCiale
+    background
     description
     slug
     image {
