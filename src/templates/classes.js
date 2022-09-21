390f36e4480daa4ee54c5f52f59b006292c0f531
anthow <anthonyenglebert@gmail.com>
2022-09-21 13:43:10 +0200

    /

--- src/templates/classes.js
+++ src/templates/classes.js
@@ -9,18 +9,18 @@ const CLassePage  = ({ data })  => (
 
   <Layout>
     <Seo title="Home" />
-   <article className=" w-10/12 m-auto space-x-20 flex justify-center text-white">
-   <GatsbyImage image={data.datoCmsClasse.image.gatsbyImageData} className="w-1/3" />        
+   <article className=" w-10/12 m-auto space-x-20 grid grid-cols-2 justify-center text-white">
+   <GatsbyImage image={data.datoCmsClasse.image.gatsbyImageData} className="w-full" />        
 
   <section>
     <h1 className="text-3xl font-black text-red-600">{data.datoCmsClasse.nomDeLaClasse}</h1>
     <h2 className="text-2xl font-black text-yellow-600"> Background</h2>
-    <div className="w-1/2 mb-10" dangerouslySetInnerHTML={{ __html: data.datoCmsClasse.background }} />
+    <div className="mb-10" dangerouslySetInnerHTML={{ __html: data.datoCmsClasse.background }} />
 
     <h2 className="text-2xl font-black text-yellow-600"> capacité spéciale</h2>
     <h3 className="text-xl font-black text-white my-5"> {data.datoCmsClasse.nomDeLaCapacitSpCiale}</h3>
 
-    <div className="w-1/2" dangerouslySetInnerHTML={{ __html: data.datoCmsClasse.description }} />
+    <div className="" dangerouslySetInnerHTML={{ __html: data.datoCmsClasse.description }} />
     <div className="mt-5" dangerouslySetInnerHTML={{ __html: data.datoCmsClasse.dTailCapacitSpCiale }} />
 
   </section>
